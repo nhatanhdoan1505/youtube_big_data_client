a352ecae7b0ecd166664f7c8d45eb77bca1b9fca
Doan Nhat Anh <anhfernaldo@gmail.com>
2021-11-21 21:35:21 +0700

    change ip server

--- api/axiosClient.ts
+++ api/axiosClient.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  //baseURL: "http://149.28.153.126:8080/api",
-  baseURL: "http://localhost:8080/api",
+  baseURL: "http://149.28.153.126:8080/api",
+  //baseURL: "http://localhost:8080/api",
   headers: { "Content-Type": "application/json" },
 });
 
