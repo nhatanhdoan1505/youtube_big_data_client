4dd31024a4783a65a15bb43261522ef9d9f8bde9
Doan Nhat Anh <anhfernaldo@gmail.com>
2021-11-22 18:57:37 +0700

    change sv ip

--- api/axiosClient.ts
+++ api/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http:///45.76.159.187/:8080/api",
+  baseURL: "http://45.76.159.187:8080/api",
   //baseURL: "http://localhost:8080/api",
   headers: { "Content-Type": "application/json" },
 });
