d56c9113bac4d11c8314f8911153dfed38f27e0a
Doan Nhat Anh <anhfernaldo@gmail.com>
2022-07-18 17:33:01 +0700

    fix bug

--- hook/index.ts
+++ hook/index.ts
@@ -1,6 +1,5 @@
 export * from "./useSystemStat";
 export * from "./useHotBase";
-export * from "./useChannelOverview";
 export * from "./useChannelTags";
 export * from "./useVideoViewsDistribution";
 export * from "./useVideoHotListBase";
